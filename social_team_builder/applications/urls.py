e60c41333fbcc6cf1b20584cc4762d4b98622724
CrossroadsMan <crossroadsman@gmail.com>
2020-01-24 16:33:21 -0700

    Create new 'applications' app, split applications-related views/templates out of 'app' app

--- social_team_builder/applications/urls.py
+++ social_team_builder/applications/urls.py
@@ -19,7 +19,7 @@ from django.urls import re_path
 from . import views
 
 
-app_name = 'app'
+app_name = 'applications'
 urlpatterns = [
-    re_path(r'^$', views.index, name='index'),
+    re_path(r'^applications', views.applications, name="applications"),
 ]
